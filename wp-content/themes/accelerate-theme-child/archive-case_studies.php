3c6d9bc0394ad9ef5a359bc0a1c4f77abc3b484a
CNicole <christine@dotcomdesigns.biz>
2019-07-03 13:29:12 -0500

    adding single case studies

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the archive page for case studies
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post(); 
+				$image_1 = get_field("image_1");
+				$size = "medium";
+				$services = get_field('services');
+			?>
+			<article class="case_study">
+					<aside class="case-study-sidebar">
+						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?>"</a></h2>
+						<h5> <?php echo $services; ?> </h5>
+						
+						
+						<?php the_excerpt(); ?>
+						
+						<p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project > </a></p>
+					</aside>
+					<div class="case-study-images">
+					<?php if($image_1) { 
+							echo wp_get_attachment_image( $image_1, $size );
+						 } ?>
+					</div>
+		
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+			
+			
+		</div><!-- .main-content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
